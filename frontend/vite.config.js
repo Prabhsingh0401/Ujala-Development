fb3ad482ac8b6a29ca148ebc464d274627d914b5
Prableen Singh Matharu <130837233+Prabhsingh0401@users.noreply.github.com>
2025-11-08 16:39:24 +0530

    build fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,18 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
   ],
+  build: {
+    outDir: 'dist'  
+  },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
+      // ❗ Remove localhost backend
+      // If you have a backend, you should call the deployed API instead
     },
   },
 })
